f30a1a0e429673f980427869e2065e64132e6428
Eric D. Helms <ericdhelms@gmail.com>
2025-06-10 08:22:10 -0400

    Set release-version instead of SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def jetty-version "9.4.57.v20241219")
 
-(defproject org.clojars.ehelms/trapperkeeper-webserver-jetty9 "4.5.3-SNAPSHOT"
+(defproject org.clojars.ehelms/trapperkeeper-webserver-jetty9 "4.5.3-1"
   :description "A jetty9-based webserver implementation for use with the puppetlabs/trapperkeeper service framework."
   :url "https://github.com/puppetlabs/trapperkeeper-webserver-jetty9"
   :license {:name "Apache License, Version 2.0"
